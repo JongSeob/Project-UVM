909636161c6bf245c2a6a75379091329e95ca32d
gyuyeol <lgy307@gmail.com>
2022-01-26 11:45:40 +0900

    test

--- example/1.driver-dut/part_2.sv
+++ example/1.driver-dut/part_2.sv
@@ -13,7 +13,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 //----------------------------------------------------------------------
-
+//test
 // First Steps with UVM - The DUT Interface
 // See https://youtu.be/FkclDiK4Oco
 
