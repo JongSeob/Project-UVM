7625332c661eaa6876950fbd37d3b36f7c6b25fa
Jongseob Yun <cjsgkwkd4@gmail.com>
2022-02-09 12:08:43 +0900

    delete unused line in reg_mode example

--- example/4.reg_model/Chipverify_regModel_2_Complete_UVCs.sv
+++ example/4.reg_model/Chipverify_regModel_2_Complete_UVCs.sv
@@ -380,7 +380,6 @@ class my_env extends uvm_env;
     // and set default map of the register env
     virtual function void connect_phase (uvm_phase phase);
         super.connect_phase (phase);
-        m_reg_env.m_agent = m_agent;
         m_agent.m_mon.mon_ap.connect (m_reg_env.m_apb2reg_predictor.bus_in);
         m_reg_env.m_ral_model.default_map.set_sequencer(m_agent.m_seqr, m_reg_env.m_reg2apb);
     endfunction
