635e69ec71fb2224469a3e51b0ff427be0b3aec9
gyuyeol <lgy307@gmail.com>
2022-02-15 22:40:39 +0900

    update

--- Gyuyeol/my_driver.sv
+++ Gyuyeol/my_driver.sv
@@ -1,25 +1,6 @@
 
 `include "sequence_item.sv"
 
-// Declare a sequence_item for the APB transaction 
-class bus_pkt extends uvm_sequence_item;
-    rand bit [31:0]  addr;
-    rand bit [31:0]  data;
-    rand bit         write;
-
-    `uvm_object_utils_begin (bus_pkt)
-        `uvm_field_int (addr, UVM_ALL_ON)
-        `uvm_field_int (data, UVM_ALL_ON)
-        `uvm_field_int (write, UVM_ALL_ON)
-    `uvm_object_utils_end
-
-    function new (string name = "bus_pkt");
-        super.new (name);
-    endfunction
-    
-    constraint c_addr { addr inside {0, 1, 2};}
-endclass
-
 // Drives a given apb transaction packet to the APB interface
 class my_driver extends uvm_driver #(bus_pkt);
     `uvm_component_utils (my_driver)
